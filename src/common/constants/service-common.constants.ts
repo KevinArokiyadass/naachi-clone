13e4d865aa903ba365f54f2989ff775b499ef700
Kaushik-hub77 <Kaushiknagaraj77@gmail.com>
2025-11-18 11:02:38 +0530

    Update service-common.constants.ts to remove trailing slash from admin-dev URL

--- src/common/constants/service-common.constants.ts
+++ src/common/constants/service-common.constants.ts
@@ -2,7 +2,7 @@ export const Whitelists = [
     'http://localhost:3000',
     'http://localhost:4200',
     'http://localhost:3002',
-    'https://admin-dev.naachichat.com/'
+    'https://admin-dev.naachichat.com'
 ]
 
 export const ASIA_CALCUTTA_TIMEZONE = 'Asia/Calcutta';
