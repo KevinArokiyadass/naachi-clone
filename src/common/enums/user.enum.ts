c3bf26d5007763b59a0055501f8930b40bbd3103
KaushikNoukha <Kaushik@Noukha.in>
2025-12-10 16:04:58 +0530

    fix/enum

--- src/common/enums/user.enum.ts
+++ src/common/enums/user.enum.ts
@@ -2,7 +2,6 @@ export enum AdminRoles {
   SUPER_ADMIN = 'SUPER_ADMIN',
   INSTITUTIONADMIN="INSTITUTION_ADMIN"
 }
-
 export interface IMetaTag {
   institutionsId: string;
   departmentsId: string[];
